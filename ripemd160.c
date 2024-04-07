2548f46812901bdfc783f2034a2b3e2ad1337d66
JelleSakkers <jelle.sakkers@student.uva.nl>
2024-04-07 19:58:59 +0200

    -

--- ripemd160.c
+++ ripemd160.c
@@ -1,8 +1,15 @@
+/* Naam: Jelle SAakkers
+ * UvAnetID: 14619946
+ * Opleiding: Bachelor Informatica
+ */
+
 #include <stdlib.h>
 #include <stdint.h>
 
 #define RIPEMD160_BLOCK_LENGTH 64
 #define RIPEMD160_DIGEST_LENGTH 20
+
+/* RIPEMD-160 DEFINITIONS */
 
  #define A(x, y, z) ((x) ^ (y) ^ (z))
  #define B(x, y, z) (((x) & (y)) | (~(x) & (z)))
